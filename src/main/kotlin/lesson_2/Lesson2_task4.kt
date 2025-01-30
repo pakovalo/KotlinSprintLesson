835fcf615d2f2e4fcc6533c82d26b0b7fbb8fa39
paket <pakovalo@gmail.com>
2025-01-31 00:18:02 +0300

    KSL-2-4 task2-4 mobile game done

---
+++ src/main/kotlin/lesson_2/Lesson2_task4.kt
@@ -0,0 +1,13 @@
+package org.example.lesson_2
+
+fun main () {
+
+    val ironOre = 11
+    val cristallineOre = 7
+    val bonusIronOre = (ironOre*0.2).toInt()
+    val bonusCristallineOre = (cristallineOre*0.2).toInt()
+
+    println("Колличество бонусной железной руды: $bonusIronOre")
+    println("Колличество бонусной кристалической руды: $bonusCristallineOre")
+
+}
