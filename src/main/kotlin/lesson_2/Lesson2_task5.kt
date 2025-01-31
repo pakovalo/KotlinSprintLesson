a9a37996b861953e71e49b8732b40409e019d2d0
paket <pakovalo@gmail.com>
2025-01-31 21:57:47 +0300

    KSL-2-5 task2-5 done

---
+++ src/main/kotlin/lesson_2/Lesson2_task5.kt
@@ -0,0 +1,15 @@
+package org.example.lesson_2
+
+import kotlin.math.pow
+
+fun main () {
+
+    val deposit = 70000
+    val percent = 16.7
+    val durationOfDeposit = 20
+    val finalDeposite = deposit*((1+(percent/100)).pow(durationOfDeposit))
+    val depositeToPrint = String.format("Итоговая сумма: %.3f", finalDeposite)
+
+    print(depositeToPrint)
+
+}
