d6a62487e5bec731b256961012e030ba79479c67
paket <pakovalo@gmail.com>
2025-02-01 08:56:32 +0300

    KS-2-1 task2-1 corrected

--- src/main/kotlin/lesson_2/Lesson2_task1.kt
+++ src/main/kotlin/lesson_2/Lesson2_task1.kt
@@ -8,7 +8,7 @@ fun main () {
     val studentScore3 = 3
     val studentScore4 = 5
     val sumScore = (studentScore1 + studentScore2 + studentScore3 + studentScore4).toFloat()
-    val averageScore = (sumScore/numbersOfStudents)
+    val averageScore = (sumScore / numbersOfStudents)
 
     print("Средний бал: $averageScore")
 
