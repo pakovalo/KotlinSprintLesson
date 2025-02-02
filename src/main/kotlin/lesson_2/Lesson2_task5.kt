9e91bb2de682db2ff8b5bc6169f3755161d01f3b
paket <pakovalo@gmail.com>
2025-02-02 23:10:55 +0300

    KS-2-5 task2-5 corrected

--- src/main/kotlin/lesson_2/Lesson2_task5.kt
+++ src/main/kotlin/lesson_2/Lesson2_task5.kt
@@ -7,7 +7,7 @@ fun main () {
     val deposit = 70000
     val percent = 16.7
     val durationOfDeposit = 20
-    val finalDeposite = deposit*((1 + (percent / ALL_PERCENT)).pow(durationOfDeposit))
+    val finalDeposite = deposit * ((1 + (percent / ALL_PERCENT)).pow(durationOfDeposit))
     val depositeToPrint = String.format("Итоговая сумма: %.3f", finalDeposite)
 
     print(depositeToPrint)
