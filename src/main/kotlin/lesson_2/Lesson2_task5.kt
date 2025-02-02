3ca0a4d09807439ddab272a4348e048c3320e0cd
paket <pakovalo@gmail.com>
2025-02-02 09:29:48 +0300

    KS-2-5 task2-5 space and const corrected

--- src/main/kotlin/lesson_2/Lesson2_task5.kt
+++ src/main/kotlin/lesson_2/Lesson2_task5.kt
@@ -7,9 +7,11 @@ fun main () {
     val deposit = 70000
     val percent = 16.7
     val durationOfDeposit = 20
-    val finalDeposite = deposit*((1+(percent/100)).pow(durationOfDeposit))
+    val finalDeposite = deposit*((1 + (percent / ALL_PERCENT)).pow(durationOfDeposit))
     val depositeToPrint = String.format("Итоговая сумма: %.3f", finalDeposite)
 
     print(depositeToPrint)
 
 }
+
+const val ALL_PERCENT = 100
