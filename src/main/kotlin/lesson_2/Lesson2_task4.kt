aefa3125992c2bfc04a52a899e365b137fc02775
paket <pakovalo@gmail.com>
2025-02-02 09:21:49 +0300

    KS-2-4 task2-4 space and const corrected

--- src/main/kotlin/lesson_2/Lesson2_task4.kt
+++ src/main/kotlin/lesson_2/Lesson2_task4.kt
@@ -5,10 +5,12 @@ fun main () {
     val ironOre = 11
     val cristallineOre = 7
     val buffPercent = 20
-    val bonusIronOre = (ironOre*buffPercent/100)
-    val bonusCristallineOre = (cristallineOre*buffPercent/100)
+    val bonusIronOre = (ironOre*buffPercent / ALL_PERCENT)
+    val bonusCristallineOre = (cristallineOre*buffPercent / ALL_PERCENT)
 
     println("Колличество бонусной железной руды: $bonusIronOre")
     println("Колличество бонусной кристалической руды: $bonusCristallineOre")
 
 }
+
+const val ALL_PERCENT = 100
