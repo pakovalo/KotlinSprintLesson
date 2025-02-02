5304e645c4ff5ebf7746c2ffe591c62b78e1448b
paket <pakovalo@gmail.com>
2025-02-02 15:33:12 +0300

    KS-3-1 task3-1 greeting text done

---
+++ src/main/kotlin/lesson_3/Lesson3_task1.kt
@@ -0,0 +1,23 @@
+package org.example.lesson_3
+
+import java.time.LocalTime
+
+fun main() {
+
+    val name = "Александр"
+    val day = LocalTime.of(9, 0)
+    val night = LocalTime.of(20, 0)
+    val currentTime = LocalTime.now()
+    val greetings: String
+
+    if (currentTime < night && currentTime > day) {
+        greetings = "Добрый день"
+    } else {
+        greetings = "Добой ночи"
+    }
+
+    val greetingsText = "$greetings, $name!"
+
+    print(greetingsText)
+
+}
