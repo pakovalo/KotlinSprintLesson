7cd43643f57c0705885f8e6b45b00e6cb02587b5
paket <pakovalo@gmail.com>
2025-02-02 08:37:18 +0300

    KS-2-3 task2-3 const corrected

--- src/main/kotlin/lesson_2/Lesson2_task3.kt
+++ src/main/kotlin/lesson_2/Lesson2_task3.kt
@@ -6,12 +6,12 @@ fun main () {
 
     val departureTime = LocalTime.of(9, 39)
     val travelTimeInMinutes = 457
-    val travelTimeHours = (travelTimeInMinutes / minuteInHours).toLong()
-    val travelTimeMinutes = (travelTimeInMinutes % minuteInHours).toLong()
+    val travelTimeHours = (travelTimeInMinutes / MINUTES_IN_HOUR).toLong()
+    val travelTimeMinutes = (travelTimeInMinutes % MINUTES_IN_HOUR).toLong()
     val arrivalTime = departureTime.plusHours(travelTimeHours).plusMinutes(travelTimeMinutes)
 
     print("Время прибытия поезда: $arrivalTime")
 
 }
 
-const val minuteInHours = 60
+const val MINUTES_IN_HOUR  = 60
