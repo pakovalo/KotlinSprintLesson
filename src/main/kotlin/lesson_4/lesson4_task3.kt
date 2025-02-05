ccda5dc30df3ec047dbd1937df3d4a925824f986
paket <pakovalo@gmail.com>
2025-02-05 20:53:34 +0300

    KS-4-3 task4-3 agrocomplex corrected

--- src/main/kotlin/lesson_4/lesson4_task3.kt
+++ src/main/kotlin/lesson_4/lesson4_task3.kt
@@ -4,10 +4,14 @@ fun main() {
 
     val weatherToday: Boolean = true
     val awningNow: Boolean = true
-    val airHumidity: Int = 20
+    val airHumidityNow: Int = 20
     val season: String = "зима"
-    val conditions = weatherToday == true && awningNow == true && airHumidity ==20 && season != "зима"
+    val conditions = weatherToday == SUN_WEATHER && awningNow == AWNING
+            && airHumidityNow == AIR_HUMIDITY && season != SEASON
 
     print("Благоприятные ли условия для роста бобовых? $conditions")
-
 }
+const val SUN_WEATHER: Boolean = true
+const val AWNING: Boolean = true
+const val SEASON: String = "зима"
+const val AIR_HUMIDITY = 20
