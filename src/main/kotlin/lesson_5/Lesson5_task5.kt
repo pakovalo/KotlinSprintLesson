07520ef015c336ecb4486379d54fc1940f0051c4
paket <pakovalo@gmail.com>
2025-02-07 21:45:22 +0300

    KS-5-5 task5-5 lottery new done

---
+++ src/main/kotlin/lesson_5/Lesson5_task5.kt
@@ -0,0 +1,31 @@
+package org.example.lesson_5
+
+fun main (){
+
+    val randomNumber1 = (1..42).random()
+    val randomNumber2 = (1..42).random()
+    val randomNumber3 = (1..42).random()
+    val randomNumber = listOf(randomNumber1, randomNumber2, randomNumber3)
+
+
+    println("Введите первое число от 0 до 42")
+    val yourNumbeer1 = readln().toInt()
+
+    println("Введите второе число от 0 до 42")
+    val yourNumbeer2 = readln().toInt()
+
+    println("Введите третье число от 0 до 42")
+    val yourNumbeer3 = readln().toInt()
+
+    val yourNumbers = listOf(yourNumbeer1, yourNumbeer2, yourNumbeer3)
+    val result = yourNumbers.intersect(randomNumber)
+    val guessedNumbers =result.size
+
+    when (guessedNumbers) {
+        3 -> println("Вы угадали все числа и выиграли джекпот!\n")
+        2 -> println("Вы угадали два числа и выиграли крупный приз!\n")
+        1 -> println("Вы угадали одно число и выиграли утешительный приз!\n")
+        0 -> println("Увы, совпадений нет!\n")
+    }
+    println("Выигрышные числа $randomNumber!")
+}
