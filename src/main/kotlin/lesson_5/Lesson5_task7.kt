43fc99a46edd82564338ca2b11b6db213d62afb1
paket <pakovalo@gmail.com>
2025-02-08 23:51:37 +0300

    KS-5-7 task5-7 fuel done

---
+++ src/main/kotlin/lesson_5/Lesson5_task7.kt
@@ -0,0 +1,23 @@
+package org.example.lesson_5
+
+fun main() {
+
+    val fuelFlowDivider = 100
+
+    println("Введите расстояние поездки в киллометрах:")
+    val distance = readln().toDouble()
+
+    println("Введите расход топлива на 100км (в литрах):")
+    val fuelConsumption = readln().toFloat()
+
+    println("Введите цену за литр топлива:")
+    val fuelPrice = readln().toFloat()
+
+    val fuelSpent = distance * fuelConsumption / fuelFlowDivider
+    val roundedFuelSpent = String.format("%.2f", fuelSpent)
+    val costOfFuel = fuelSpent * fuelPrice
+    val roundedCostOfFuel = String.format("%.2f", costOfFuel)
+
+    println("Необходимо топлива: $roundedFuelSpent \nСтоимость поездки: $roundedCostOfFuel")
+
+}
