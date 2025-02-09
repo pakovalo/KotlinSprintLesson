19b194259cc58a993bc439bfc953f61d792104dc
paket <pakovalo@gmail.com>
2025-02-09 21:47:06 +0300

    KS-6-1 timer corrected

--- src/main/kotlin/lesson_6/Lesson6_task1.kt
+++ src/main/kotlin/lesson_6/Lesson6_task1.kt
@@ -1,29 +1,24 @@
 package org.example.lesson_6
 
 fun main() {
-    var notCorrectedLogin: Boolean = true
 
     println("Веедите логин своего будущего аккаунта")
-    val login = readln().toString()
+    val login = readln()
 
     println("Веедите пароль")
-    val password = readln().toString()
+    val password = readln()
 
     println("Ваш аккаунт зарегестрирован!")
 
-    while (notCorrectedLogin) {
+    do {
         println("Веедите логин:")
-        val enterLogin = readln().toString()
+        val enterLogin = readln()
 
         println("Веедите Ваш пароль:")
-        val enterPassword = readln().toString()
+        val enterPassword = readln()
 
-        if (enterLogin == login && enterPassword == password) {
-            notCorrectedLogin = false
-            println("Авторизация прошла успешно")
-        } else {
-            println("Неверный логин или пароль. Попробуйте снова.")
-        }
-    }
+    } while (enterLogin != login && enterPassword != password)
+
+    println("Авторизация прошла успешно")
 
 }
