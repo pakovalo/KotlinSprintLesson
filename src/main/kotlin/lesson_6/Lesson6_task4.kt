e54a6c36bee78c3a662292424d12e4b489fa3bea
paket <pakovalo@gmail.com>
2025-02-09 22:37:35 +0300

    KS-6-4 guess number done

---
+++ src/main/kotlin/lesson_6/Lesson6_task4.kt
@@ -0,0 +1,24 @@
+package org.example.lesson_6
+
+fun main() {
+    var counter = 1
+    val maxCounter = 5
+    val numbers = (1..9).toList()
+    val winNumber = numbers.random()
+    var yourNumber = 0
+
+    while (counter <= maxCounter && yourNumber != winNumber) {
+        println("Введите число от 1 до 9")
+        yourNumber = readln().toInt()
+
+        if (yourNumber != winNumber && (maxCounter - counter) > NO_ATTEMPTS)
+            println("Неверно, осталось ${maxCounter - counter} попытки")
+
+        counter++
+    }
+    if (yourNumber == winNumber)
+        println("Это была великолепная игра!")
+    else
+        println("Неверно было загадано число $winNumber")
+}
+const val NO_ATTEMPTS = 0
