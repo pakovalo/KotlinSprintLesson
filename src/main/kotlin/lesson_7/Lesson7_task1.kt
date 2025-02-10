ee7926e5454d19d7e1bfdf6f8eac41cc49018a70
paket <pakovalo@gmail.com>
2025-02-10 17:24:37 +0300

    KS-7-1 pass generator done

---
+++ src/main/kotlin/lesson_7/Lesson7_task1.kt
@@ -0,0 +1,15 @@
+package org.example.lesson_7
+
+fun main() {
+    val passwordLenght = 16
+    val numberForPassword: IntRange = 1..2
+    val lettersForPassword: CharRange = 'a'..'z'
+    var password = ""
+
+    for (i in 1..passwordLenght step (CHARACTERS_PER_CYCLE)) {
+        password = password + lettersForPassword.random()+numberForPassword.random()
+    }
+    println(password)
+}
+
+const val CHARACTERS_PER_CYCLE = 2
