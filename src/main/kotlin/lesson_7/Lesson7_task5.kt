a34437614eea5cd352d01c4ed40a3cb77d717986
paket <pakovalo@gmail.com>
2025-02-11 20:28:02 +0300

    KS-7-5 corrected

--- src/main/kotlin/lesson_7/Lesson7_task5.kt
+++ src/main/kotlin/lesson_7/Lesson7_task5.kt
@@ -6,20 +6,22 @@ fun main() {
     val uppercaseLetter = 'A'..'Z'
     val numbers = 1..9
     var password = ""
+    val fullSymbols = lowercaseLetter + uppercaseLetter + numbers
 
     println("Введите число символов в пароле. пароль должен быть не меннее 6 символов")
     var numberOfCharacters = readln().toInt()
 
     if (numberOfCharacters < MIN_PASSWORD) {
         do {
+
             println("Введите число символов в пароле. пароль должен быть не меннее 6 символов")
             numberOfCharacters = readln().toInt()
+
         } while (numberOfCharacters < MIN_PASSWORD)
     }
-    for (i in 1..numberOfCharacters) {
-        val randomiser = listOf(lowercaseLetter.random(), uppercaseLetter.random(), numbers.random()).random()
-        password = password + randomiser
-        }
+
+    for (i in 1..numberOfCharacters)
+        password = password + fullSymbols.random()
 
     println(password)
 }
