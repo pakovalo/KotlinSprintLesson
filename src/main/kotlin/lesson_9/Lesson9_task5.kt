1070594c50ae3bbe541f7702a9168dc87ae3709d
paket <pakovalo@gmail.com>
2025-02-15 18:36:04 +0300

    KS-9-5 mutableSet done

--- src/main/kotlin/lesson_9/Lesson9_task5.kt
+++ src/main/kotlin/lesson_9/Lesson9_task5.kt
@@ -7,23 +7,18 @@ fun main() {
 
     println("Ведите название ${counter}го ингредиента")
     val yourIngredient = readln()
-    val ingredients = mutableListOf(yourIngredient.lowercase())
+
+    val ingredientsMutableSet = mutableSetOf(yourIngredient.lowercase())
     counter++
 
     do {
         println("Ведите название ${counter}го ингредиента")
-        val yourIngredient = readln()
-        val searchElementLower = yourIngredient.lowercase()
-        val found = ingredients.any { it.lowercase() == searchElementLower }
-
-        if (found) {
-            println("Такой игредиент уже есть!")
-            continue
-        } else {
-        ingredients.add(yourIngredient.lowercase())
-        }
+        val yourIngredient = readln().lowercase()
+        ingredientsMutableSet.add(yourIngredient)
         counter++
     } while (MAX_INGREDIENT >= counter)
+
+    val ingredients = ingredientsMutableSet.toMutableList()
 
     ingredients.sort()
     ingredients[0] = ingredients[0].replaceFirstChar { char -> char.uppercase() }
