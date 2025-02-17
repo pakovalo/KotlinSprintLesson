963d2fb1d0746ac46d3c495adfeb3fd72e0a1796
paket <pakovalo@gmail.com>
2025-02-17 23:22:25 +0300

    KS-11-1 print class done

---
+++ src/main/kotlin/lesson_11/Class.kt
@@ -0,0 +1,8 @@
+package org.example.lesson_11
+
+class User(
+    val id: Int,
+    val login: String,
+    val password: String,
+    val email: String,
+)
---
+++ src/main/kotlin/lesson_11/lesson11_task1.kt
@@ -0,0 +1,22 @@
+package org.example.lesson_11
+
+fun main() {
+
+    val user1 = User(
+        id=1,
+        login = "user1",
+        password = "password1",
+        email = "user1@gmail.com",
+        )
+
+    val user2 = User(
+        id = 2,
+        login = "user2",
+        password = "password2",
+        email = "user2@gmail.com"
+    )
+
+    println("User 1: ID=${user1.id}, Login=${user1.login}, Password=${user1.password}, Email=${user1.email}")
+    println("User 2: ID=${user2.id}, Login=${user2.login}, Password=${user2.password}, Email=${user2.email}")
+
+}
