9ac1ca289c25709b60a51870c4de3f3a9c1e7693
paket <pakovalo@gmail.com>
2025-02-19 15:20:56 +0300

    KS-11-4 recipe done

--- src/main/kotlin/lesson_11/Class.kt
+++ src/main/kotlin/lesson_11/Class.kt
@@ -75,3 +75,26 @@ class Room(
         }
     }
 }
+
+class Category(
+    val id: Int,                    // идентификатор
+    val name: String,               // название категории
+    val imageUrl: String,           // картинка категории
+    val briefDescription: String,   // краткое описание категории
+)
+
+class Ingredient(
+    val id: Int,                    // идентификатор
+    val name: String,               // название ингредиента
+    val quantity: Int,              // количество
+    val unitMeasurement: String,    // единица измерения
+)
+
+class Recipe(
+    val id: Int,                        // идентификатор
+    val title: String,                  // название рецепта
+    val imageUrl: String,               // Ссылка на изображение рецепта
+    val categoryId: Int,                // Идентификатор категории, к которой относится рецепт
+    val ingredients: List<Ingredient>,  // Список ингредиентов
+    val steps: List<String>,            // Шаги приготовления
+)
