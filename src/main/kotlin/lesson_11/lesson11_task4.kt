c86277552797fe51b656362c694cd45b492328cd
paket <pakovalo@gmail.com>
2025-02-21 21:43:26 +0300

    KS-11-4 ingredient corrected

--- src/main/kotlin/lesson_11/lesson11_task4.kt
+++ src/main/kotlin/lesson_11/lesson11_task4.kt
@@ -1,24 +1,25 @@
 package org.example.lesson_11
 
 class Category(
-    val id: Int,                    // идентификатор
-    val name: String,               // название категории
-    val imageUrl: String,           // картинка категории
-    val briefDescription: String,   // краткое описание категории
+    val id: Int,
+    val name: String,
+    val imageUrl: String,
+    val briefDescription: String,
+    val Recipe: List<Recipe>
 )
 
 class Ingredient(
-    val id: Int,                    // идентификатор
-    val name: String,               // название ингредиента
-    val quantity: Int,              // количество
-    val unitMeasurement: String,    // единица измерения
+    val id: Int,
+    val name: String,
+    val quantity: Int,
+    val unitMeasurement: String,
 )
 
 class Recipe(
-    val id: Int,                        // идентификатор
-    val title: String,                  // название рецепта
-    val imageUrl: String,               // Ссылка на изображение рецепта
-    val categoryId: Int,                // Идентификатор категории, к которой относится рецепт
-    val ingredients: List<Ingredient>,  // Список ингредиентов
-    val steps: List<String>,            // Шаги приготовления
+    val id: Int,
+    val title: String,
+    val imageUrl: String,
+    val categoryId: Int,
+    val ingredients: List<Ingredient>,
+    val steps: List<String>,
 )
