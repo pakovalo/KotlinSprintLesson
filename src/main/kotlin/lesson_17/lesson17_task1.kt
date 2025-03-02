016f681d1e62031b1fb18bd464fc598b20add624
paket <pakovalo@gmail.com>
2025-03-02 15:33:01 +0300

    KS-17-1 quiz done

---
+++ src/main/kotlin/lesson_17/lesson17_task1.kt
@@ -0,0 +1,15 @@
+package org.example.lesson_17
+
+class Quiz (
+    question: String,
+    answer: String,
+) {
+    var question: String = question
+        get() = field
+
+    var answer: String = answer
+        get() = field
+        set(value) {
+            field = value
+        }
+}
