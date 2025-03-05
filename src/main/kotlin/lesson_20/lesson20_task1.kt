b290fe3596c19d5f2ea247c5bbf543d5bf47e14f
pakovalo <pakovalo@gmail.com>
2025-03-06 00:00:31 +0300

    Delete src/main/kotlin/lesson_20/lesson20_task1.kt

--- src/main/kotlin/lesson_20/lesson20_task1.kt
+++
@@ -1,7 +0,0 @@
-package org.example.lesson_20
-
-fun main() {
-    val username = "Миша"
-    val greeting: (String) -> String = { "С наступающим Новым Годом, $username!" }
-    println(greeting(username))
-}
