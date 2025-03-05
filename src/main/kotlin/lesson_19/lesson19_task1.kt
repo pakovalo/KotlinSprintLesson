55ac7109a37e2577597eb4b7b5533cbd13c22824
paket <pakovalo@gmail.com>
2025-03-05 11:23:42 +0300

    KS-19-1 fish corrected

--- src/main/kotlin/lesson_19/lesson19_task1.kt
+++ src/main/kotlin/lesson_19/lesson19_task1.kt
@@ -1,19 +1,13 @@
 package org.example.lesson_19
 
-enum class Fish {
-    GUPPY,
-    ANGELFISH,
-    GOLDFISH,
-    SIAMESE_FIGHTING_FISH,
+enum class Fish (val nameFish: String) {
+    GUPPY ("Гуппи"),
+    ANGELFISH("Скалярия"),
+    GOLDFISH("Золотая рыбка"),
+    SIAMESE_FIGHTING_FISH("Петушок"),
 }
 
 fun main() {
     println("Вы можете добавить в аквариум следующих рыб:")
-    for (fish in Fish.entries)
-        when (fish) {
-            Fish.GUPPY -> println("Гуппи")
-            Fish.ANGELFISH -> println("Скалярия")
-            Fish.GOLDFISH -> println("Золотая рыбка")
-            Fish.SIAMESE_FIGHTING_FISH -> println("Петушок")
-        }
+    Fish.entries.forEach { println("${it.nameFish}") }
 }
