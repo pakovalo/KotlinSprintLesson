fc57da226b380e4abe80134028daef9daf3756d1
paket <pakovalo@gmail.com>
2025-03-07 23:55:15 +0300

    KS-21-5 max skill done

---
+++ src/main/kotlin/lesson_21/lesson21_task5.kt
@@ -0,0 +1,17 @@
+package org.example.lesson_21
+
+fun main() {
+    val listSkills = mapOf<String,Int>(
+        "Лечение" to 100,
+        "Урон" to 110,
+        "Броня" to 150,
+        "Уворот" to 120,
+        "Сила" to 130,
+        "Ловкость" to 80,
+        "Интелект" to 150
+    )
+    println(listSkills.maxCategory())
+}
+fun Map<String, Int>.maxCategory (): String {
+return maxBy { it.value }.key
+}
